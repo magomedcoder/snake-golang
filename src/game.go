7bda279d8f9f188c7afb52c786fac9b34efb30ab
Magomedcoder <info@magomedcoder.ru>
2022-12-04 12:53:25 +0300

    Added draw cell

--- src/game.go
+++ src/game.go
@@ -1,10 +1,24 @@
 package app
 
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+)
+
 type Game struct {
 	snake *Snake
 	food  *Food
 	dir   Dir
 	input KeyboardInput
+}
+
+func (g *Game) drawBackground(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{A: 255, R: 21, G: 21, B: 20})
+	for i := 0; i < Cols; i++ {
+		for j := 0; j < Rows; j++ {
+			DrawCell(screen, i, j, color.Black, Gap, CellSize)
+		}
+	}
 }
 
 func New() *Game {
---
+++ src/utils.go
@@ -0,0 +1,13 @@
+package app
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
+)
+
+func DrawCell(screen *ebiten.Image, x, y int, c color.Color, gap, cellSize int) {
+	xPos := float64(x*cellSize + gap)
+	yPos := float64(y*cellSize + gap)
+	ebitenutil.DrawRect(screen, xPos, yPos, float64(cellSize-2*gap), float64(cellSize-2*gap), c)
+}
