f20473a74fbc953c0d81e1eb75bb9a37c077f997
Magomedcoder <info@magomedcoder.ru>
2022-12-04 12:56:51 +0300

    Added log

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	app "app/src"
 	"github.com/hajimehoshi/ebiten/v2"
+	"log"
 )
 
 func main() {
 	ebiten.SetWindowSize(app.Width, app.Height)
 	ebiten.SetWindowTitle("Змейка на Go")
 	ebiten.SetTPS(15)
-	ebiten.RunGame(app.New())
+	if err := ebiten.RunGame(app.New()); err != nil {
+		log.Fatal(err)
+	}
 }
